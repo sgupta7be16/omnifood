dd05f28887c7c6a1bc1683675f2cb3b870f84412
sgupta7be16 <43212989+sgupta7be16@users.noreply.github.com>
2019-05-09 18:17:40 +0530

    Add files via upload

---
+++ omnifood/resources/js/script.js
@@ -0,0 +1,87 @@
+$(document).ready(function()
+{
+	//for sticky navigation
+	$('.js--section-features').waypoint(function(direction)
+	{
+		if(direction == "down"){
+			$('nav').addClass('sticky');
+
+		}	
+		else{
+			$('nav').removeClass('sticky');
+		}	
+	},{offset:'60px;'});
+
+	//scroll button
+	$('.js--scroll-to-plans').click(function(){
+		$('html,body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+	});
+
+	$('.js--scroll-to-start').click(function () {
+       	$('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000); 
+    });
+
+    $('.js--our-cities').click(function () {
+       	$('html, body').animate({scrollTop: $('.js--section-cities').offset().top}, 1000); 
+    });
+
+     $('.js--food-delivery').click(function () {
+     $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000); 
+    });
+
+     $('.js--howitworks').click(function () {
+     $('html, body').animate({scrollTop: $('.js--section-steps').offset().top}, 1000); 
+    });
+
+     $('.js--sign-up').click(function () {
+   	$('html, body').animate({scrollTop: $('.js--section-form').offset().top}, 1000); 
+	});
+
+   //aminations on scroll
+   $('.js--wp-1').waypoint(function(direction){
+   	$('.js--wp-1').addClass('animated fadeIn');
+   },{
+   	offset:'50%;'
+   });
+
+   $('.js--wp-2').waypoint(function(direction){
+   	$('.js--wp-2').addClass('animated fadeInUp');
+   },{
+   	offset:'50%;'
+   });
+
+   $('.js--wp-3').waypoint(function(direction){
+   	$('.js--wp-3').addClass('animated fadeIn');
+   },{
+   	offset:'50%;'
+   });
+
+   $('.js--wp-4').waypoint(function(direction){
+   	$('.js--wp-4').addClass('animated pulse');
+   },{
+   	offset:'50%;'
+   });
+
+   ///////mobile mavigation
+   $('.js--nav-icon').click(function(){
+   		var nav = $('.js--main-nav')
+   		var icon = $('.js--nav-icon i')
+   		nav.slideToggle(200);
+   		if(icon.hasClass('ion-navicon-round')){
+   			icon.addClass('ion-close-round')
+   			icon.removeClass('ion-navicon-round');
+   		}
+   		else{
+   			icon.addClass('ion-navicon-round')
+   			icon.removeClass('ion-close-round');
+
+   		}
+
+
+
+   });
+
+
+});
+
+  
